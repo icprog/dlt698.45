1f180055f5fe2b2cca4467a1227c445e6803906f
qq <fzh@fan-notebook.(none)>
2017-01-07 10:52:38 +0800

    485抄表代码增加

--- cjdeal/read485.c
+++ cjdeal/read485.c
@@ -16,6 +16,18 @@
 #include <errno.h>
 #include "read485.h"
 
+typedef struct
+{
+	INT8U missionID;		//参数变量接口类逻辑名
+	TI interval;						//执行频率
+	INT16U deepsize;					//存储深度
+	INT8U  cjtype;						//采集类型
+	DataType data;
+	CSD    csd[20];						//记录列选择 array CSD,
+	MS     ms;							//电能表集合
+	INT8U  savetimeflag;				//存储时标选择 enum
+}CLASS_6013;//任务配置单元
+
 void read485_thread()
 {
   while(1){
@@ -24,7 +36,30 @@ void read485_thread()
   pthread_detach(pthread_self());
   pthread_exit(&thread_read485);
 }
+/*
+ * 根据6013任务配置单元去文件里查找对应的6015普通采集方案
+ * 输入 st6013
+ * 输出 st6015
+*/
+INT8U use6013find6015(CLASS_6013 st6013, CLASS_6015* st6015)
+{
+	INT8U result = 0;
 
+	return result;
+
+}
+/*
+ * 具体抄读6015中配置的数据项
+ * 输入 st6013
+ * 输出 st6015
+*/
+INT8U deal6015_698protocol(CLASS_6015 st6015)
+{
+	INT8U result = 0;
+
+	return result;
+
+}
 void read485_proccess()
 {
 	pthread_attr_init(&read485_attr_t);
@@ -32,6 +67,13 @@ void read485_proccess()
 	pthread_attr_setdetachstate(&read485_attr_t,PTHREAD_CREATE_DETACHED);
 	while ((thread_read485_id=pthread_create(&thread_read485, &read485_attr_t, (void*)read485_thread, NULL)) != 0)
 	{
+
+
+		CLASS_6013 from6013;
+		CLASS_6015 to6015;
+		memset(&to6015,0,sizeof(CLASS_6015));
+		INT8U ret = use6013find6015(from6013,&to6015);
+
 		sleep(1);
 	}
 }
--- cjdeal/read485.h
+++ cjdeal/read485.h
@@ -7,7 +7,7 @@
 
 #ifndef READ485_H_
 #define READ485_H_
-
+#include "Objectdef.h"
 pthread_attr_t read485_attr_t;
 int thread_read485_id;           //485、四表合一（I型、II型、专变）
 pthread_t thread_read485;
