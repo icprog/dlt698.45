76964eee8c5bf3c6e68e88b9290722c89e14160e
xuxiaopeng <xuxiaopeng@qq>
2017-01-11 14:23:27 +0800

    add secure file

---
+++ lib698/secure.h
@@ -0,0 +1,14 @@
+/*
+ * secure.h
+ *
+ *  Created on: 2017-1-11
+ *      Author: gk
+ */
+
+#ifndef SECURE_H_
+#define SECURE_H_
+
+ INT16S secureGetAppDataUnit(INT8U* apdu,INT8U** appData);
+
+
+#endif /* SECURE_H_ */
