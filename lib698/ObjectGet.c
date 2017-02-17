b198d64eb2fefc29957406248fbbec9482ae96bc
xuxiaopeng <xuxiaopeng@qq>
2017-02-17 15:34:50 +0800

    esam response mingwen jiami

--- lib698/ObjectGet.c
+++ lib698/ObjectGet.c
@@ -21,9 +21,11 @@ extern INT8U Get_Event(OI_698 oi,INT8U eventno,INT8U** Getbuf,INT8U *Getlen);
 extern void getoad(INT8U *data,OAD *oad);
 extern int get_BasicRCSD(INT8U *source,CSD_ARRAYTYPE *csds);
 extern int get_BasicRSD(INT8U *source,INT8U *dest,INT8U *type);
+extern INT16S composeSecurityResponse(INT8U* SendApdu,INT16U Length);
 extern int comfd;
 extern INT8U TmpDataBuf[MAXSIZ_FAM];
 extern INT8U TmpDataBufList[MAXSIZ_FAM*2];
+extern INT8U securetype;
 typedef struct
 {
 	OAD oad;
@@ -121,9 +123,11 @@ int BuildFrame_GetResponse(INT8U response_type,CSINFO *csinfo,INT8U oadnum,RESUL
 	memcpy(&sendbuf[index],response.data,response.datalen);
 	index = index + response.datalen;
 	sendbuf[index++] = 0;
-
-//	apduplace += SecureApdu(&sendbuf[apduplace],index-apduplace);
-
+	if(securetype!=0)//安全等级类型不为0，代表是通过安全传输下发报文，上行报文需要以不低于请求的安全级别回复
+	{
+		apduplace += composeSecurityResponse(&sendbuf[apduplace],index-apduplace);
+		index=apduplace;
+	}
 	FrameTail(sendbuf,index,hcsi);
 	if(pSendfun!=NULL)
 		pSendfun(comfd,sendbuf,index+3);
