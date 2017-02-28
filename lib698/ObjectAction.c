869477ea8fb14053e24001fb25a4bca1b70f6b50
lhl <lhl@qq.com>
2017-02-28 14:30:20 +0800

    test1

--- lib698/ObjectAction.c
+++ lib698/ObjectAction.c
@@ -17,6 +17,7 @@
 #include "secure.h"
 #include "basedef.h"
 
+//1 liuhongli
 void get_BasicUnit(INT8U *source,INT16U *sourceindex,INT8U *dest,INT16U *destindex);
 extern INT8U Reset_add();
 extern void FrameTail(INT8U *buf,int index,int hcsi);
