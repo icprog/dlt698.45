27cc19d35aed94b4dc37b684088788702117d662
ZhouLiHai <i@lihai.space>
2017-03-06 17:02:18 +0800

    修正文件传输功能

--- lib698/filetrans.c
+++ lib698/filetrans.c
@@ -36,83 +36,75 @@ static unsigned short blocksize;
 static unsigned char checksumtmp;
 static unsigned char file_path[256];
 
+int createFile(const char* path, int length, unsigned char crc, unsigned short bs) {
+    FILE* fp = NULL;
+    //文件不能太长
+    if (length > 5 * 1024 * 1024) {
+        return 112;
+    }
+    checksum    = crc;
+    checksumtmp = 0x00;
+    blocksize   = bs;
 
-int createFile(const char * path, int length, unsigned char crc, unsigned short bs){
-	FILE 	*fp=NULL;
-	//文件不能太长
-	if(length > 5 * 1024 * 1024){
-		return 112;
-	}
-	checksum = crc;
-	checksumtmp = 0x00;
-	blocksize = bs;
+    //文件夹不存在,创建文件夹
+    if (access("/nand/UpFiles", F_OK) != 0) {
+        system("mkdir /nand/UpFiles");
+    }
+    memcpy(file_path, path, sizeof(file_path));
 
-	//文件夹不存在,创建文件夹
-	if(access("/nand/UpFiles", F_OK)!=0){
-		system("mkdir /nand/UpFiles");
-	}
-	memcpy(file_path, path, sizeof(file_path));
+    //打开文件，并写入空值对文件进行填充
+    fp = fopen((const char*)path, "w+");
+    if (fp != NULL) {
+        INT8U fills[1024];
+        fseek(fp, 0L, SEEK_SET);
+        int len = length;
+        while (len > 0) {
+            int num = fwrite(fills, (len > 1024) ? 1024 : len, 1, fp);
+            //			printf("create File size:%d-%d-%d\n", num, errno, length);
+            len -= (len > 1024) ? 1024 : len;
+        }
+        fclose(fp);
 
-	//打开文件，并写入空值对文件进行填充
-	fp = fopen((const char*)path, "w+");
-	if (fp != NULL) {
-		INT8U fills[1024];
-		fseek(fp, 0L, SEEK_SET);
-		int len = length;
-		while(len >0){
-			int num = fwrite(fills,(len>1024)?1024:len,1,fp);
-//			printf("create File size:%d-%d-%d\n", num, errno, length);
-			len -= (len>1024)?1024:len;
-		}
-		fclose(fp);
+    } else {
+        return 105;
+    }
 
-	}
-	else{
-		return 105;
-	}
-
-	return 0;
+    return 0;
 }
 
+int appendFile(int shift, int length, unsigned char* buf) {
+    FILE* fp = NULL;
 
-int appendFile(int shift, int length, unsigned char *buf){
-	FILE 	*fp=NULL;
+    //文件不存在
+    if (access(file_path, F_OK) != 0) {
+        return 104;
+    }
 
-	//文件不存在
-	if(access(file_path, F_OK)!=0){
-		return 104;
-	}
+    //打开文件，并写入空值对文件进行填充
+    fp = fopen((const char*)file_path, "r+");
+    if (fp != NULL) {
+        fseek(fp, shift * blocksize, SEEK_SET);
+        fwrite(buf, length, 1, fp);
+        fclose(fp);
+        for (int j = 0; j < length; j++) {
+            checksumtmp += buf[j];
+        }
+        struct stat mstats;
+        stat(file_path, &mstats);
+        int res = (int)((shift * blocksize + length) * 100.0) / mstats.st_size;
+        if (res == 100) {
+            printf("checksumtmp = %d-%d\n", checksumtmp, checksum);
+            if (checksumtmp == checksum) {
+                return 100;
+            } else {
+                return 107;
+            }
+        } else {
+            return res;
+        }
+    } else {
+        return 105;
+    }
 
-	//打开文件，并写入空值对文件进行填充
-	fp = fopen((const char*)file_path, "r+");
-	if (fp != NULL) {
-		fseek(fp, shift*blocksize, SEEK_SET);
-		fwrite(buf,length,1,fp);
-		fclose(fp);
-		for(int j = 0; j < length; j++){
-			checksumtmp += buf[j];
-		}
-		struct stat mstats;
-		stat(file_path, &mstats);
-		int res = (int)((shift*blocksize + length)*100.0)/mstats.st_size;
-		if(res == 100){
-			printf("checksumtmp = %d-%d\n", checksumtmp, checksum);
-			if(checksumtmp == checksum){
-				return 100;
-			}
-			else{
-				return 107;
-			}
-		}
-		else
-		{
-			return res;
-		}
-	}
-	else{
-		return 105;
-	}
-
-	return 0;
+    return 0;
 }
-
