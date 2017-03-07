4fe31569704c5dc8f6e939d89d57110a31e520d2
ZhouLiHai <i@lihai.space>
2017-03-07 10:47:36 +0800

    加入对事件序号校验的功能，方式共享内存出错。

--- cjcomm/cjcomm.c
+++ cjcomm/cjcomm.c
@@ -20,6 +20,10 @@ static CLASS25 Class25;
 
 void EventAutoReport(CommBlock* nst) {
     static int local_index = 0;
+    if (JProgramInfo->needreport_event.event_num >= 16 || JProgramInfo->needreport_event.event_num < 0) {
+        asyslog(LOG_ERR, "事件上报模块发生严重错误！事件序号越限(%d)", JProgramInfo->needreport_event.event_num);
+        return;
+    }
     if (local_index != JProgramInfo->needreport_event.event_num) {
         //循环存储16个
         local_index += 1;
