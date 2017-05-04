0296d7c34d92c4e21da84313bfe9b4f4145c3f1c
ZhouLiHai <i@lihai.space>
2017-05-04 18:01:56 +0800

    升级包增加了md5校验，并且make增加了参数

--- updatetool/make.sh
+++ updatetool/make.sh
@@ -1,51 +1,96 @@
 #!/bin/sh
-#ROOTDIRSRC=/home/Administrator/jzq_gw_20121119
-ROOTDIRSRC=$(pwd)
-APPDIRSRC=$ROOTDIRSRC
-SHNAME=update.sh
-SHNAMEPATH=$ROOTDIRSRC/$SHNAME
-DIROBJ=/nand/bin
-LIBOBJ=/nor/lib
-TMPDIR=/nand/UpFiles
-ls
-rm -rf cjgwn
-rm -f update.sh
-rm -f $APPDIRSRC/app.tar.gz
-rm ./app/*
-cp ../bin_arm/cj* ./app/
-cp ../bin_arm/*.so ./app/
-cp ../config/* ./app/
-tar -cvf app.tar ./app
-gzip -9 app.tar
-rm -f $SHNAMEPATH
-cd $ROOTDIRSRC
-echo "#!/bin/sh" > $SHNAMEPATH
-echo "mkdir -p "$TMPDIR"" >> $SHNAMEPATH
-echo "(read l;read l;read l;read l;read l;read l;read l;read l;read l;read l;read l;exec cat ) <  $SHNAME | gunzip -  | tar -xvf - -C $TMPDIR && ls -l /nand/bin" >>  $SHNAMEPATH
-echo "cp "$TMPDIR"/app/cj* /nand/bin/" >>$SHNAMEPATH
-echo "cp "$TMPDIR"/app/lib* /nor/lib/" >> $SHNAMEPATH
-echo "cp "$TMPDIR"/app/*.cfg /nor/config/" >> $SHNAMEPATH
-echo "===" >> $SHNAMEPATH
-echo "chmod +x "$DIROBJ"/*"  >> $SHNAMEPATH
-echo "echo update finished!!!" >> $SHNAMEPATH
-echo "rm -rf app" >> $SHNAMEPATH
-echo "exit" >> $SHNAMEPATH
-cat  $APPDIRSRC/app.tar.gz >> $SHNAMEPATH
-#rm -f $APPDIRSRC/app.tar.gz
-chmod +x  $SHNAMEPATH
 
-mkdir cjgwn
-mkdir cjgwn/app
-cp update.sh ./cjgwn/app/
-cp rc.local ./cjgwn/
-cd app ;md5sum * > ../QCheck/md5.ini; cd ..
-KI=`md5sum clean.sh | cut -d " " -f1`
-rm update.sh
-cat needed/head >> update.sh
-echo 'md5='$KI >> update.sh
-cat needed/tail >> update.sh
-cat app.tar.gz >> update.sh
+Usage()
+{
+    echo "============================================="
+    echo "使用方式:"
+    echo "\t./make 型号 地区\n"
+    echo "\t设备类型，1：I型集中器，2:II型集中器，3：III型专变"
+    echo "\tZheJiang(II型),HuNan(I型)"
+    echo "============================================="
+    exit 1
+}
 
-echo "cp /dos/cjgwn/app/update.sh /nand/UpFiles/update.sh" >> "./cjgwn/index.sh"
-echo "cp /dos/cjgwn/rc.local /nor/rc.d/rc.local" >> ./cjgwn/index.sh
-echo "echo reboot >> /nand/UpFiles/reboot" >> ./cjgwn/index.sh
+ParaCheck()
+{
+    if [ "$#" -ne "2" ]; then
+        Usage
+    fi
+
+    if [ $1 -ne "1" ] && [ $1 -ne "2" ] && [ $1 -ne "3" ]; then
+        Usage
+    fi
+
+    if [ $2 != "ZheJiang" ] && [ $2 != "HuNan" ]; then
+         Usage
+    fi
+
+    echo "创建必要条件..."
+    if [ ! -d app ]; then
+        mkdir app
+    fi
+}
+
+Clean()
+{
+    echo "清理文件..."
+    rm -rf cjgwn
+    rm -rf app.tar.gz
+    rm -rf update.sh
+}
+CopyNew()
+{
+    echo "复制最新的程序与库..."
+    cp ../bin_arm/cj* ./app/
+    cp ../bin_arm/*.so ./app/
+    cp ../config/* ./app/
+}
+
+Package()
+{
+    echo "打包自解压文件..."
+    tar -cvf app.tar ./app
+    gzip -9 app.tar
+
+    KI=`md5sum app.tar.gz | cut -d " " -f1`
+    cat needed/head >> update.sh
+    echo 'md5='$KI >> update.sh
+    cat needed/tail >> update.sh
+    cat app.tar.gz >> update.sh
+    cp update.sh ./cjgwn/app/
+}
+
+CreateUSB()
+{
+    echo "创建U盘升级包..."
+    mkdir cjgwn
+    mkdir cjgwn/app
+    cp needed/rc.local ./cjgwn/
+    cp needed/index.sh ./cjgwn/
+}
+
+Tools()
+{
+    echo "更新生产检测脚本..."
+    cd app ;md5sum * > ../QCheck/md5.ini; cd ..
+}
+
+Post_Clean()
+{
+    echo "执行清理..."
+    rm app.tar.gz
+    rm -rf app
+}
+
+main()
+{
+    ParaCheck $1 $2
+    Clean
+    CopyNew
+    Package
+    CreateUSB
+    Tools
+    Post_Clean
+}
+
+main $1 $2
