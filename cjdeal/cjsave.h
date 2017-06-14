fc7db99538321d0eacd5b3da1b5f0e96614a4c80
cxl <cxl>
2017-06-14 11:38:32 +0800

    485跨日创建任务文件

--- cjdeal/cjsave.h
+++ cjdeal/cjsave.h
@@ -11,5 +11,5 @@
 
 extern int SaveNorData(INT8U taskid,ROAD *road_eve,INT8U *databuf,int datalen,TS ts_cc);
 //extern void ReadNorData(TS ts,INT8U taskid,INT8U *tsa);
-
+extern void CreateSaveHead(char *fname,ROAD *road_eve,CSD_ARRAYTYPE csds,INT16U *headlen,INT16U *unitlen,INT16U *unitnum,INT16U freq,INT8U wrflg);
 #endif /* CJSAVE_H_ */
