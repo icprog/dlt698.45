8775d9e8b28f81bd624cbb4403c475ecac93fa48
fanzhihong <fzh@123.com>
2017-08-04 23:15:20 +0800

    载波测试ttyS2->ttyS5

--- cjdeal/readplc.c
+++ cjdeal/readplc.c
@@ -828,7 +828,7 @@ int doInit(RUNTIME_PLC *runtime_p)
 			if (runtime_p->comfd >0)
 				CloseCom( runtime_p->comfd );
 
-			runtime_p->comfd = OpenCom(2, 9600,(unsigned char*)"even",1,8);// 5 载波路由串口 ttyS5     // 维护串口 2 ttyS2
+			runtime_p->comfd = OpenCom(5, 9600,(unsigned char*)"even",1,8);// 5 载波路由串口 ttyS5     // 维护串口 2 ttyS2
 			DbgPrintToFile1(31,"comfd=%d",runtime_p->comfd);
 			runtime_p->initflag = 0;
 			clearvar(runtime_p);//376.2上行内容容器清空，发送计时归零
