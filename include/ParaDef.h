c31fec7c4bf50cf673acba4ad3f5010898540f06
lhl <lhl@qq.com>
2017-08-08 11:29:00 +0800

    zb test

--- include/ParaDef.h
+++ include/ParaDef.h
@@ -189,13 +189,13 @@
 #define S4851   		1
 #define S4852   		2
 #define S4853   		3
-#define SER_ZB		5
+#define SER_ZB			5
 
 //国网送检：测试载波模块互换性，将载波口换做4851,来测试载波模块互换性功能
-//#define S4851   	5//1
-//#define S4852   	2
+//#define S4851   	1
+//#define S4852   	5//2
 //#define S4853   	3
-//#define SER_ZB		1//5
+//#define SER_ZB		2//5
 
 //  I型集中器串口定义
 //　　　GPRS  [ /dev/ttyS0 ]
