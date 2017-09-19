29a2deb71f9cdf961843edb8a23349b17b444217
lhl <lhl@qq.com>
2017-09-19 15:30:02 +0800

    8106_act127

--- lib698/dlt698.h
+++ lib698/dlt698.h
@@ -100,6 +100,7 @@ extern int getBitString(INT8U type, INT8U* source, INT8U* dest);                
 extern int getDouble(INT8U* source, INT8U* dest);                                 // 5 6
 extern int getOctetstring(INT8U type, INT8U* source, INT8U* tsa,INT8U *DAR);                 // 9 and 0x55
 extern int getVisibleString(INT8U *source,INT8U *dest,INT8U *DAR);                          // 0x0A
+extern int getInteger(INT8U *source,INT8S *dest,INT8U *DAR);                     // 0x0F
 extern int getUnsigned(INT8U *source,INT8U *dest,INT8U *DAR);                     // 0x11
 extern int getLongUnsigned(INT8U* source, INT8U* dest);                           // 0x12
 extern int getLong64(INT8U *source,INT64U *dest);									//0x14
