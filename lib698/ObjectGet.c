3cacd4e863f1063a8ff54ef50f6c5735e9261a2c
lhl <lhl@qq.com>
2017-09-30 09:15:59 +0800

    8107_set

--- lib698/ObjectGet.c
+++ lib698/ObjectGet.c
@@ -131,7 +131,7 @@ int BuildFrame_GetResponseNext(INT8U response_type,CSINFO *csinfo,INT8U DAR,INT1
 
 	csinfo->dir = 1;
 	csinfo->prm = 1;
-//	csinfo->gframeflg = 1;		//分帧标志
+//	csinfo->gframeflg = 1;		//不能使用分帧标志，否则是一帧
 
 	index = FrameHead(csinfo,sendbuf);
 	hcsi = index;
