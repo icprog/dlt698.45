ac0990b84d48c7a189bb1da28f819c8bde9ba58f
cxl <cxl>
2017-10-21 14:31:02 +0800

    存储优化代码

--- cj/data.c
+++ cj/data.c
@@ -27,111 +27,6 @@ void ReadFileHeadLen(FILE *fp,INT16U *headlen,INT16U *blocklen)
 	fread(&blocklength,2,1,fp);
 	*blocklen = ((blocklength>>8)+((blocklength&0xff)<<8));
 }
-/*
- * 读取抄表数据，读取某个测量点某任务某天一整块数据，放在内存里，根据需要提取数据,并根据csd
- */
-//void ReadNorData(TS ts,INT8U taskid,INT8U *tsa)
-//{
-//	FILE *fp  = NULL;
-//	INT16U headlen=0,unitlen=0,unitnum=0,readlen=0;
-//	INT8U *databuf_tmp=NULL;
-//	INT8U *headbuf=NULL;
-//	int i=0,j=0;
-//	int savepos=0;
-//	TS ts_now;
-//	TSGet(&ts_now);
-//	char	fname[128]={};
-//	TASKSET_INFO tasknor_info;
-//	if(ReadTaskInfo(taskid,&tasknor_info)!=1)
-//		return;
-//	getTaskFileName(taskid,ts_now,fname);
-//	fp = fopen(fname,"r");
-//	if(fp == NULL)//文件没内容 组文件头，如果文件已存在，提取文件头信息
-//	{
-//		fprintf(stderr,"\nopen file %s fail\n",fname);
-//		return;
-//	}
-//	ReadFileHeadLen(fp,&headlen,&unitlen);
-//	headbuf = (INT8U *)malloc(headlen);
-//	unitnum = (headlen-4)/sizeof(HEAD_UNIT);
-//	ReadFileHead(fp,headlen,unitlen,unitnum,headbuf);
-//	databuf_tmp = malloc(unitlen);
-//	fseek(fp,headlen,SEEK_SET);//跳过文件头
-//	while(!feof(fp))//找存储结构位置
-//	{
-//		//00 00 00 00 00 00 00 00 07 05 00 00 00 00 00 01
-//		//00 00 00 00 00 00 00 00 00 07 05 00 00 00 00 00 01
-//		readlen = fread(databuf_tmp,unitlen,1,fp);
-//		fprintf(stderr,"\n----读出来的长度readlen=%d,块大小unitlen=%d\n",readlen,unitlen);
-//		if(readlen == 0)
-//			break;
-//		fprintf(stderr,"\n");
-//		fprintf(stderr,"\n传进来的TSA：");
-//		for(i=0;i<TSA_LEN;i++)
-//			fprintf(stderr," %02x",tsa[i]);
-//		fprintf(stderr,"\n文件里的TSA：");
-//		for(i=0;i<TSA_LEN;i++)
-//			fprintf(stderr," %02x",databuf_tmp[i+1]);
-//		if(memcmp(&databuf_tmp[1],&tsa[0],17)==0)//找到了存储结构的位置，一个存储结构可能含有unitnum个单元
-//		{
-//			savepos=ftell(fp)-unitlen;//对应的放到对应的位置
-//			break;
-//		}
-//	}
-//	fprintf(stderr,"\n----(pos:%d):\n",savepos);
-//	if(savepos==0)//没找到的不打印
-//		return;
-//	fprintf(stderr,"\n要求的tsa:");
-//	for(j=0;j<17;j++)
-//		fprintf(stderr,"%02x ",tsa[j]);
-//	fprintf(stderr,"\n文件中的数据(pos:%d):",savepos);
-//	fseek(fp,savepos,SEEK_SET);//
-//	fread(databuf_tmp,unitlen,1,fp);
-////		for(j=0;j<unitlen;j++)
-////		{
-////			fprintf(stderr,"%02x ",databuf_tmp[j]);
-////		}
-//	fprintf(stderr,"\n---存储单元数：%d 每个单元长度:%d\n",tasknor_info.runtime,unitlen/tasknor_info.runtime);
-//	fprintf(stderr,"\n文件%s存储的数据(%d)：",fname,unitlen);
-//	fprintf(stderr,"\n");
-//
-//	for(i=0;i<unitlen;i++)
-//	{
-//		if(i%(unitlen/tasknor_info.runtime) == 0)
-//			fprintf(stderr,"\n%d:",i/(unitlen/tasknor_info.runtime));
-//
-//		fprintf(stderr," %02x",databuf_tmp[i]);
-//	}
-//
-//}
-//void cjread(int argc, char *argv[])
-//{
-//	INT8U tsa[17];
-//	INT8U taskid=0;
-//	TS ts_now;
-//	int len=0,i=0,tmp=0;
-//	fprintf(stderr,"\nargc =%d",argc);
-//	if (argc>3)
-//	{
-//		len = argc - 3;//cj test 05 02 23 34 54 67
-//		sscanf(argv[2],"%d",&tmp);
-//		if(tmp >= 256)
-//			return;
-//		if (len>18)
-//			return;
-//		taskid = tmp;
-//		memset(tsa,0,17);
-//		for(i=0;i<len;i++)
-//		{
-//			sscanf(argv[i+3],"%02x",&tmp);
-//			tsa[i] = (INT8U)tmp;
-//		}
-//		TSGet(&ts_now);
-//		ReadNorData(ts_now,taskid,tsa);
-//	}
-//}
-
-
 int findtsa(FILE *fp,int *TSA_D,int A_TSAblock)
 {
 	INT8U tmp=0,buf[20]={};
@@ -171,16 +66,16 @@ int findtsa(FILE *fp,int *TSA_D,int A_TSAblock)
 		}
 		if (findok==0)
 		{
-			fseek(fp,begitoffset+A_TSAblock,0);
+			fseek(fp,begitoffset+A_TSAblock+TSA_LEN+1,0);
 		}else
 		{
-			fseek(fp,begitoffset,0);
+			fseek(fp,begitoffset+TSA_LEN+1,0);
 			break;
 		}
 	}
 	return findok;
 }
-void record_prt(int recordnum,int indexn,HEAD_UNIT0 *length,FILE *fp)
+void record_prt(int recordnum,int indexn,HEAD_UNIT *length,FILE *fp)
 {
 	int	nonullflag=0;
 	int k=0,i=0,j=0;
@@ -416,9 +311,10 @@ void analyTaskData(int argc, char* argv[])
 	int TSA_D[20]={};
 	char *filename= argv[2];
 	FILE *fp=NULL;
-	int i=0, indexn=0,A_record=0,A_TSAblock=0;
-	HEAD_UNIT0 length[20];
-	int tsanum=0 , head_len=0,recordnum =0,haveTsa =0 , unitnum=0;
+	int i=0;
+	int tsanum=0 , head_len=0,haveTsa =0;
+	HEADFIXED_INFO taskhead_info;
+	HEAD_UNIT headoad_unit[FILEOADMAXNUM];
 
 	if (filename!=NULL)
 	{
@@ -426,7 +322,7 @@ void analyTaskData(int argc, char* argv[])
 		{
 			tsanum = argc -3;
 			haveTsa = tsanum;   //人工输入的TSA目标字节数
-			fprintf(stderr,"\n\n\n【 目标地址: ");
+			fprintf(stderr,"\n\n\n【 目标地址(%d): ",tsanum);
 			for(i=0;i<tsanum;i++)
 			{
 				sscanf(argv[i+3],"%02x",&TSA_D[i]);
@@ -439,24 +335,20 @@ void analyTaskData(int argc, char* argv[])
 		if(fp!=NULL)
 		{
 			fprintf(stderr,"\n\n\n--------------------------------------------------------");
-			head_len = readfile_int(fp);
-			fprintf(stderr,"\n文件头长度 %d (字节)",head_len);
+//			head_len = readfile_int(fp);
+			fread(&taskhead_info,sizeof(HEADFIXED_INFO),1,fp);
+			fread(headoad_unit,taskhead_info.oadnum*sizeof(HEAD_UNIT),1,fp);
+			head_len = sizeof(HEADFIXED_INFO)+taskhead_info.oadnum*sizeof(HEAD_UNIT);
 
-			A_TSAblock = readfile_int(fp);
-			memset(&length,0,sizeof(length));
-			unitnum = (head_len )/sizeof(HEAD_UNIT0);
+			fprintf(stderr,"\n文件头长度:%d (字节) 对象oad总数:%d 记录长度:%d 每日记录次数:%d 记录间隔:%d(秒)\n",
+					head_len,taskhead_info.oadnum,taskhead_info.reclen,taskhead_info.seqnum,taskhead_info.seqsec);
 
-			//打印文件头结构
-			A_record = head_prt(unitnum,length,&indexn,fp);
 
-			recordnum = A_TSAblock/A_record;
-			fprintf(stderr,"\n\n\n TSA块 %d（字节）  每记录 %d （字节）  共 %d 条记录\n",A_TSAblock,A_record,recordnum);
-
-			if (findtsa(fp,TSA_D,A_TSAblock)==1)
+			if (findtsa(fp,TSA_D,taskhead_info.reclen*taskhead_info.seqnum)==1)
 			{
 				fprintf(stderr,"\n\n\n>>>>查找到相关的TSA数据(%d)\n",ftell(fp));
 				//打印TSA的全部记录
-				record_prt(recordnum,indexn,length,fp);
+				record_prt(taskhead_info.seqnum,taskhead_info.oadnum,headoad_unit,fp);
 			}
 			else
 				fprintf(stderr,"\n\n\n>>>>未查找到相关的TSA数据\n\n\n");
