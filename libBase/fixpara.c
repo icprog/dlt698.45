a5274a1be57e7f18f6a4c6a480f8e2644216eb9b
lhl <lhl@qq.com>
2017-12-15 10:27:15 +0800

    171214

--- libBase/fixpara.c
+++ libBase/fixpara.c
@@ -32,7 +32,7 @@ typedef struct {
 									//厂商代码　　软件版本　软件日期　　硬件版本　硬件日期  扩展信息
 static VERINFO verinfo          = { "QDGK", "V1.1", "171130", "1.10", "160328", "00000000" }; // 4300 版本信息
 									//湖南需要双协议,软件版本要求为SXY8（双协议8） ，1376.1（软件版本为SXY1）
-static VERINFO verinfo_HuNan    = { "QDGK", "SXY8", "171213", "1.10", "160328", "00000000" }; // 4300 版本信息
+static VERINFO verinfo_HuNan    = { "QDGK", "SXY8", "171214", "1.10", "160328", "00000000" }; // 4300 版本信息
 static VERINFO verinfo_ZheJiang  = { "QDGK", "V1.1", "171207", "1.10", "160328", "00000000" }; // 4300 版本信息
 static VERINFO verinfo_ShanDong  = { "QDGK", "V1.2", "171026", "1.10", "160328", "00000000" }; // 4300 版本信息
 
